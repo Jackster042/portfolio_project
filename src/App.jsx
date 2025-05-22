a1f3942ea35894583c4b34854881a8020c140006
Nemanja Stojanovic <feemail042@gmail.com>
2025-05-22 22:46:57 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold underline cursor-pointer text-indigo-600">
+        Portfolio Project
+      </h1>
+    </div>
+  );
+};
+
+export default App;
